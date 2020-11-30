ddb1dce9de0cb207f9fb8d1a207f3427fe91f9e7
Dongjoon Kim <dongjoon.kim@rakuten.com>
2020-11-30 17:19:30 +0900

    load an image

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -2,22 +2,31 @@ package com.example.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit;
     TextView text;
+    ImageView imageView;
+    Bitmap bitmap;
+
     String key = "9a2cdb92482fb060bcdc3bec358a713e";
     String data;
 
@@ -28,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         edit = (EditText) findViewById(R.id.edit);
         text = (TextView) findViewById(R.id.result);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 
     public void mOnClick(View v) {
@@ -37,12 +47,11 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         data = getJsonData();
-                        run();
-
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 text.setText(data);
+                                imageView.setImageBitmap(bitmap);
                             }
                         });
                     }
@@ -55,9 +64,10 @@ public class MainActivity extends AppCompatActivity {
         StringBuffer buffer = new StringBuffer();
         String str = edit.getText().toString();
         String location = URLEncoder.encode(str);
+
+        //String queryUrl="http://api.weatherstack.com/current?access_key="+key+"&query="+location;
         String queryUrl = "http://api.weatherstack.com/current?access_key=111453242708ba78c89a6250c4b07d83&query=" + location;
         try {
-            URL url = new URL(queryUrl);
             InputStream is = null;
             is = new URL(queryUrl).openStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, "UTF-8"));
@@ -66,11 +76,19 @@ public class MainActivity extends AppCompatActivity {
             if (str1 == null) {
                 buffer.append("not found");
             }
-            //buffer.append(str2);
             JSONObject obj = new JSONObject(str1);
             JSONObject obj2 = obj.getJSONObject("current");
             int temp = obj2.getInt("temperature");
+           // String icon = obj2.getString("weather_icons");
+           // String encodedurl = URLEncoder.encode(icon,"UTF-8");
+           // System.out.println(encodedurl);
+            URL url2 = new URL("https://assets.weatherstack.com//images//wsymbols01_png_64//wsymbol_0006_mist.png");
+           // URL url2 = new URL(encodedurl);
+            //System.out.println(url2);
+            bitmap = BitmapFactory.decodeStream(url2.openConnection().getInputStream());
             System.out.println(temp);
+            //System.out.println(icon);
+            System.out.println(bitmap);
             if (temp != 0) {
                 buffer.append("The temperature in " + location.toUpperCase() + " is " + temp + "°c");
             }
@@ -80,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             System.out.println("error?");
         }
-
 
         return buffer.toString();
     }
